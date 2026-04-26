e6e846ee57745e76299667d636ebd82b7cec3852
Vennela2413 <elaproluvennela24@gmail.com>
2026-04-26 18:32:33 +0530

    fixed line endings

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://swasthyavani-1.onrender.com" });
+const API = axios.create({ baseURL: "https://swasthyavani-1.onrender.com/api" });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("aidoctor_token");
